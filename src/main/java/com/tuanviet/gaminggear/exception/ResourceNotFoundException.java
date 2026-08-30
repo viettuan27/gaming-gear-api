097198c3267952c81a76338220f0514152f741af
viettuan27 <trantuanviet27052003@gmail.com>
2026-08-31 00:47:01 +0700

    ApiResponse va Exception

---
+++ src/main/java/com/tuanviet/gaminggear/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.tuanviet.gaminggear.exception;
+
+public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException(String message){
+        super(message);
+    }
+}
